adb35dcdbc37f0762c6875bce19a4b62ac8fefd1
Marrior-dot <mrlimaroberto@gmail.com>
2021-06-05 15:22:45 -0300

    index e app trabalhando na funcionalidade do botão com id button e do div com id images

---
+++ app.js
@@ -0,0 +1,7 @@
+let button = document.getElementById("testStart");
+
+button.addEventListener("click",function(){
+	let test = document.getElementById("images");
+	test.style.display = 'flex';
+	console.log(test);
+} )
