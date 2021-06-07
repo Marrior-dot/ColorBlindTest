6c8f8d72f12fa8163201bffe8f5479acc423cbbe
Marrior-dot <mrlimaroberto@gmail.com>
2021-06-07 11:26:41 -0300

    nada a ser implementado

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ let button = document.getElementById("testStart");
 
 button.addEventListener("click",function(){
 	let test = document.getElementById("images");
+	let result = document.getElementById("results");
+	result.style.display = 'flex';
 	test.style.display = 'flex';
+	button.style.display = 'none';
 	console.log(test);
 } )
